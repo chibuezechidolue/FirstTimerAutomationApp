8f934d03a618d2d3cedf9ed22fb86b9e0f113ba4
Stanley Chidolue <chidoluechibueze@gmail.com>
2024-08-12 11:05:16 +0100

    created a tabulate result function that tabulate input to a spreadsheet using pygsheets module

---
+++ attendance_tabulator.py
@@ -0,0 +1,73 @@
+import pygsheets 
+import os
+import time
+
+import pygsheets.address
+
+
+def insert_cell_value(worksheet,cell_label,value,col_num,addr_list):
+    current_value=worksheet.get_value(addr=f"{cell_label}{col_num}")
+    if current_value== "":
+            val_to_update=value
+    else:
+        val_to_update=f"{current_value} {value}"
+    if cell_label!=addr_list[1]:
+        worksheet.cell(f"{cell_label}{col_num}").set_horizontal_alignment(pygsheets.custom_types.HorizontalAlignment.CENTER)
+    if cell_label==addr_list[3]:
+        worksheet.cell(f"{cell_label}{col_num}").set_number_format(pygsheets.custom_types.FormatType.TEXT) 
+    worksheet.update_value(addr=f"{cell_label}{col_num}", val=val_to_update, parse=None)
+     
+def check_if_phone_no(value):
+    try:
+        int(value)
+    except:
+        return False
+    return True
+
+def tabulate_result(first_timer_info:list,sheet_name,client,date,addr_list:list):
+    """To transfer and tabulate the first_timer_info to an online google sheet"""
+    # client.create('First Timers Attendance')
+    # client.drive.delete('First Timers Attendance')
+    spreadsht = client.open(sheet_name) 
+    # spreadsht.share('chinwechidolue@gmail.com', role='writer', type='user', emailMessage='Here is the spreadsheet we talked about!')
+    
+    worksht = spreadsht.worksheet("title", "Sheet1") 
+
+    adr=pygsheets.address.Address(value=addr_list[0], allow_non_single=True)
+    adr_index=adr.index[1]
+    col=worksht.get_col(col=adr_index)  # To get the list of values in Col 
+    col_num=col.index("")+1
+    title=["TTP FIRST TIMER REGISTER",date]
+    for n in range(len(title)):
+        worksht.merge_cells(start=f"{addr_list[0]}{col_num+n}", end=f"{addr_list[-1]}{col_num+n}", merge_type='MERGE_ROWS', grange=None)
+        worksht.cell(f"{addr_list[0]}{col_num+n}").value= title[n]
+        worksht.cell(f"{addr_list[0]}{col_num+n}").set_horizontal_alignment(pygsheets.custom_types.HorizontalAlignment.CENTER )
+        worksht.cell(f"{addr_list[0]}{col_num+n}").set_text_format(attribute="fontSize", value=16)
+        worksht.cell(f"{addr_list[0]}{col_num+n}").set_text_format(attribute="bold", value=True)
+
+    col_titles=['S/N','Name','Gender','Phone no']
+    col_label=addr_list
+    for n in range(len(col_titles)):
+        worksht.cell(f"{col_label[n]}{col_num+2}").value= col_titles[n]   
+        worksht.cell(f"{col_label[n]}{col_num+2}").set_horizontal_alignment(pygsheets.custom_types.HorizontalAlignment.CENTER )
+        worksht.cell(f"{col_label[n]}{col_num+2}").set_text_format(attribute="fontSize", value=12)
+        worksht.cell(f"{col_label[n]}{col_num+2}").set_text_format(attribute="bold", value=True)    
+
+    col_num+=3
+    for n in range(len(first_timer_info)):
+        label=" "
+        for info in first_timer_info[n]:
+            if len(info)>1 and check_if_phone_no(info):
+                label=addr_list[3]
+            elif len(info)>1 and not check_if_phone_no(info):
+                label=addr_list[1]
+            elif len(info)==1:
+                label=addr_list[2]
+                if info.upper()=='M':
+                    info="MALE"
+                elif info.upper()=='F':
+                    info="FEMALE"
+
+            worksht.cell(f"{addr_list[0]}{col_num+n}").value= n+1
+            worksht.cell(f"{addr_list[0]}{col_num+n}").set_horizontal_alignment(pygsheets.custom_types.HorizontalAlignment.CENTER)
+            insert_cell_value(worksheet=worksht,cell_label=label,value=info,col_num=col_num+n,addr_list=addr_list)
